d221b9214f103034beff75e72549903dc6947f84
vieiraigor8787 <igor_vc@hotmail.com>
2019-07-23 15:19:50 -0300

    asynAwait

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-alert('alo aloooo...')
+const myPromessa = () => new Promise((resolve, reject) =>{
+    setTimeout(() => { resolve('OK') }, 2000)
+})
+
+//com 'them':
+// myPromessa()
+//     .then(response =>{
+//         console.log(response);
+    // myPromessa()
+    //     .then(response =>{
+    //         console.log(response);
+        // myPromessa()
+        //     .then(response =>{
+        //         console.log(response);
+//     })
+//     .catch(err => {{
+
+//     }})
+
+/*async function executaPromise(){
+    console.log(await myPromessa())
+    console.log(await myPromessa())
+    console.log(await myPromessa())
+}*/
+
+// arrow Function
+const executaPromise = async () => {
+    console.log(await myPromessa())
+    console.log(await myPromessa())
+    console.log(await myPromessa())
+}
+executaPromise()
